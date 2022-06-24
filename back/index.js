7a17729b859023818c908da5a951c393c9aff38e
abgailcruz <engr.gail.cruz@gmail.com>
2022-06-23 22:22:59 -0600

    Today's changes

--- back/index.js
+++ back/index.js
@@ -2,32 +2,36 @@ import express from "express";
 import bodyParser from "body-parser";
 import cors from "cors";
 
+
+
 // IMPORT ROUTES
 import registersRoute from "./routes/register.js";
+import workspacesRoute from "./routes/workspaces.js";
+
 
 const app = express();
 const PORT = 4000;
 app.use(bodyParser.json());
 app.use(cors());
 
+
+
 // all register routers will be start with this route /register
 app.use("/registers", registersRoute);
+app.use("/workspaces", workspacesRoute);
+
 
 app.get("/", (req, res) => {
-	res.send("Server working!");
+    res.send("Server working!");
 });
+
 
 app.use((error, req, res, next) => {
-	res.status(400).json({
-		status: "error",
-		message: error.message
-	});
+    res.status(400).json({
+        status: "error",
+        message: error.message
+    });
 });
 
-app.get("/workspaces", (req, res) => {
-	res.json({
-		status: "ok",
-	});
-})
 
 app.listen(PORT, () => console.log(`Running on port: http://localhost:${PORT}`));
