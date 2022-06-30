cff47beac6a2e5a23c75c37d98e914667870c7de
abgailcruz <engr.gail.cruz@gmail.com>
2022-06-30 01:23:00 -0600

    Changes made

--- front/scripts/components/modal.js
+++ front/scripts/components/modal.js
@@ -1,4 +1,4 @@
-/* @name: Course Project: Shared WorkspaceWeb App
+/* @name: Course Project: Shared Workspace Web App
  * @Course Code: SODV1201 - 22MAYMNOT4
  * @class: Introduction to Web Programming 
  * @authors: Abigail Cruz, Debora Salles Antunes, Jorge Alberto Hurtado Ortega, Maria Estrella
