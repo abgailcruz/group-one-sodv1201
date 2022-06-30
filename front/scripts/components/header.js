db9f338a2780a27d245dfe7e80d2fd91aae3236b
abgailcruz <engr.gail.cruz@gmail.com>
2022-06-30 04:52:29 -0600

    Front-end comments and other minor changes added

--- front/scripts/components/header.js
+++ front/scripts/components/header.js
@@ -4,18 +4,30 @@
  * @authors: Abigail Cruz, Debora Salles Antunes, Jorge Alberto Hurtado Ortega, Maria Estrella
  */
 
+/* Header Components:
+ * Right: Register, Profile, and Sign out
+ * Left: Clickable logo to "/"
+ */
+
+// Function for the Logout button click
 function signOff() {
 	window.localStorage.removeItem("loginWorkspace");
 	window.location.href = "/front/login.html";
 }
 
+/* Function to change the header depending on when the user is logged in or not.
+ * Logged in: Will show the Profile and the Logout Button.
+ * Else: Will show the login button.
+ */
 function menuLogin() {
 	if (localStorage.getItem("loginWorkspace")) {
-		return '<a href="/front/user-profile.html">Profile</a> <button class="btn-sign-off" onClick="signOff()">Sign off</button>';
+		return '<a href="/front/user-profile.html">Profile</a><button class="btn-sign-off" onClick="signOff()">Logout</button>';
 	}
 	return '<a href="/front/login.html">Login</a>';
 }
 
+// The template for the header:
+// (The error will show below the header if, for example, there is an incorrect user input.)
 const HEADER = `<div class="header__nav">
 	<div class="header__left">
 		<a href="/" class="header__logo">BV Workspaces</a>
@@ -30,4 +42,5 @@ const HEADER = `<div class="header__nav">
 	</div>
 `;
 
+// Append the header(above) to the html pages:
 $("#header").append(HEADER);
